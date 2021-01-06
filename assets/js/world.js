636e8ee1e97e0d61a779fb48aa52502107605efc
Ruby <s110619049@stu.ntue.edu.tw>
2021-01-07 02:47:58 +0800

    updates

---
+++ assets/js/world.js
@@ -0,0 +1,28 @@
+// The plugin code
+(function($){
+    $.fn.parallax = function(options){
+      var $$ = $(this);
+      offset = $$.offset();
+      var defaults = {
+        'start': 0,
+        'stop': offset.top + $$.height(),
+        'coeff': 0.95
+      };
+      var opts = $.extend(defaults, options);
+      return this.each(function(){
+        $(window).bind('scroll', function() {
+          windowTop = $(window).scrollTop();
+          if((windowTop >= opts.start) && (windowTop <= opts.stop)) {
+            newCoord = windowTop * opts.coeff;
+            $$.css({
+                'background-position': '0 '+ newCoord + 'px'
+            });
+          }
+        });
+      });
+    };
+  })(jQuery);
+  
+  // call the plugin
+  $('#one1').parallax({ 'coeff':-0.65 });
+  $('#one1 .inner').parallax({ 'coeff':1.15 });
